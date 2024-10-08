613c1874f8ed4824ab386328d90d88360d9b59bc
AlenaSmile <127399848+AlenaSmile@users.noreply.github.com>
2024-10-08 10:55:25 +0300

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
     return ( 
         <View style = { styles.container } >
-            <Text style = { styles.text } > Hi!My name is(ваше имя) < /Text>
+            <Text style = { styles.text } > Hi!My name is(ваше имя) </Text>
         </View>
     );
 }
