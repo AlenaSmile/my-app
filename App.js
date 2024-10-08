920395609165c0f2dff5f7b1c155b452e68cfbfe
AlenaSmile <alenaeliseenko2003@gmail.com>
2024-10-08 12:33:36 +0300

    App.js

--- App.js
+++ App.js
@@ -1,24 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-    return ( 
-        <View style = { styles.container } >
-            <Text style = { styles.text } > Hi!My name is(ваше имя) </Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hi! My name is Alena</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#3498db', // Замените на понравившийся цвет
-    },
-    text: {
-      color: 'white',
-      fontSize: 24,
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#483D8B',
+  },
+  text: {
+    color: 'white',
+    fontSize: 24,
+  },
 });
