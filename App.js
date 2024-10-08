0ffb09c0fc24669cc78b2d990efff924e1695689
AlenaSmile <alenaeliseenko2003@gmail.com>
2024-10-08 10:29:19 +0300

    App.js package-lock.json package.json

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return ( <
+        View style = { styles.container } >
+        <
+        Text style = { styles.text } > Hi!My name is(ваше имя) < /Text> <
+        /View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#3498db', // Замените на понравившийся цвет
+    },
+    text: {
+        color: 'white',
+        fontSize: 24,
+    },
 });
