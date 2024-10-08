fe2520f5f395c16ca8a1adc0d5a9d18563c91349
AlenaSmile <127399848+AlenaSmile@users.noreply.github.com>
2024-10-08 10:32:27 +0300

    Update App.js

--- App.js
+++ App.js
@@ -3,23 +3,22 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-    return ( <
-        View style = { styles.container } >
-        <
-        Text style = { styles.text } > Hi!My name is(ваше имя) < /Text> <
-        /View>
+    return ( 
+        <View style = { styles.container } >
+            <Text style = { styles.text } > Hi!My name is(ваше имя) < /Text>
+        </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#3498db', // Замените на понравившийся цвет
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#3498db', // Замените на понравившийся цвет
     },
     text: {
-        color: 'white',
-        fontSize: 24,
+      color: 'white',
+      fontSize: 24,
     },
 });
