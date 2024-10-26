88834c7a07ae74fc27f9c2cfc68e2ac6f9aba0c3
AlenaSmile <alenaeliseenko2003@gmail.com>
2024-10-26 14:48:11 +0300

    component/LanguageItem.js data.js

---
+++ data.js
@@ -0,0 +1,19 @@
+export const languages = [{
+        id: 1,
+        name: 'JavaScript',
+        experience: '2 years of experience',
+        logo: 'https://example.com/javascript-logo.png',
+    },
+    {
+        id: 2,
+        name: 'Python',
+        experience: '1 year of experience',
+        logo: 'https://example.com/python-logo.png',
+    },
+    {
+        id: 3,
+        name: 'Java',
+        experience: '3 years of experience',
+        logo: 'https://example.com/java-logo.png',
+    },
+];
