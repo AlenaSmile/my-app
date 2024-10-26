e4dfd4bb69d256157db6cdfa00059e210efffa88
AlenaSmile <127399848+AlenaSmile@users.noreply.github.com>
2024-10-26 15:34:55 +0300

    Update data.js

--- data.js
+++ data.js
@@ -2,19 +2,19 @@ export const languages = [
   {
         id: 1,
         name: 'JavaScript',
-        experience: '2 years of experience',
-        logo: 'https://example.com/javascript-logo.png',
+        experience: '3 years of experience',
+        logo: <img src="../images/javascript.png" alt="logo">,
   },
   {
         id: 2,
         name: 'Python',
-        experience: '1 year of experience',
-        logo: 'https://example.com/python-logo.png',
+        experience: '2 year of experience',
+        logo: <img src="../images/python.png" alt="logo">,
     },
     {
         id: 3,
-        name: 'Java',
-        experience: '3 years of experience',
-        logo: 'https://example.com/java-logo.png',
+        name: 'C++',
+        experience: '1.5 years of experience',
+        logo: <img src="../images/c++.png" alt="logo">,
     },
 ];
